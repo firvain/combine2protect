478956df7e1e6c1bfb932d94d122fadca7740372
firvain <etsipis@terracognita.eu>
2019-07-09 18:21:26 +0300

    add menus to toolbar

--- client/src/components/SiteToolBar.vue
+++ client/src/components/SiteToolBar.vue
@@ -11,7 +11,60 @@
     >
     <v-spacer></v-spacer>
     <v-toolbar-items>
-      <v-btn
+      <v-btn flat small :to="pages[0].route">
+        {{ pages[0].title }}
+      </v-btn>
+      <v-btn flat small :to="pages[1].route">
+        {{ pages[1].title }}
+      </v-btn>
+      <v-menu open-on-hover offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn small flat v-on="on" :to="pages[2].route">
+            {{ pages[2].title }}
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-tile
+            v-for="(item, index) in pages[2].subpages"
+            :key="index"
+            :to="item.route"
+          >
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+      <v-btn flat small :to="pages[3].route">
+        {{ pages[3].title }}
+      </v-btn>
+      <v-btn flat small :to="pages[4].route">
+        {{ pages[4].title }}
+      </v-btn>
+      <v-menu open-on-hover offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn small flat v-on="on" :to="pages[5].route">
+            {{ pages[5].title }}
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-tile
+            v-for="(item, index) in pages[5].subpages"
+            :key="index"
+            :to="item.route"
+          >
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-menu>
+
+      <v-btn flat small :to="pages[6].route">
+        {{ pages[6].title }}
+      </v-btn>
+      <v-btn flat small :to="pages[7].route">
+        {{ pages[7].title }}
+      </v-btn>
+      <!-- <v-btn
         flat
         small
         v-for="page in pages"
@@ -19,7 +72,7 @@
         :to="page.route"
       >
         {{ page.title }}
-      </v-btn>
+      </v-btn> -->
     </v-toolbar-items>
   </v-toolbar>
 </template>
