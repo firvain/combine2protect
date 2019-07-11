257e97cc07a2c47d95d9ac4bd1f4804164d26f6a
firvain <etsipis@terracognita.eu>
2019-07-11 23:28:02 +0300

    check config

--- server/index.js
+++ server/index.js
@@ -1,28 +1,18 @@
-// const db = require("./db");
-require("dotenv-flow").config({});
+const db = require("./db");
 const express = require("express");
 const router = express.Router();
 router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
-const config = {
-  host: process.env.DBHOST,
-  port: process.env.DBPORT,
-  database: process.env.DBDATABASE,
-  user: process.env.DBUSER,
-  password: process.env.DBPASSWORD,
-  application_name: process.env.DBAPPLICATION_NAME,
-  ssl: true
-};
 router.route("/").get(function(req, res, next) {
-  res.send(config);
+  res.send("api");
 });
-// router.route("/pages").get(async (req, res, next) => {
-//   try {
-//     const data = await db.pages.all();
-//     res.send(data.map(e => e.page));
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
+router.route("/pages").get(async (req, res, next) => {
+  try {
+    const data = await db.pages.all();
+    res.send(data.map(e => e.page));
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 module.exports = router;
