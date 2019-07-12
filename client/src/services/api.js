389f5314c93fb43279eaf27a5c6428c5836471a7
firvain <etsipis@terracognita.eu>
2019-07-12 15:59:37 +0300

    fix axios baseurl

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,12 @@
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:3000/api";
+
+let url;
+if (process.env.NODE_ENV === "development") {
+  url = "http://localhost:3000/api";
+} else {
+  url = process.env.VUE_APP_API_URL;
+}
+axios.defaults.baseURL = url;
 
 export const getPages = async () => {
   try {
