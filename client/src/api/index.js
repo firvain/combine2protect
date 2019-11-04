ca4acb6ab4e45a18ee7c05926759c1da4cb45885
firvain <etsipis@terracognita.eu>
2019-11-04 17:01:18 +0200

    switch to jsonp

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,7 +3,8 @@ import axios from "axios";
 export async function fetchFeatureInfo({ url }) {
   const info = await axios({
     method: "get",
-    url
+    url,
+    responseType: "application/javascript"
   });
   return new Promise(resolve => resolve(info.data));
 }
